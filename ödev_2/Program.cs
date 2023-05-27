10d3e814eebb41dd5a783a0b8e63b81fe1111d88
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 21:31:22 +0300

    Add project files.

---
+++ ödev_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int toplam = 0;
+            for (int a = 1; a <= sayi; a++)
+            {
+                toplam += a;
+            }
+
+            Console.WriteLine("Sonuç : " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
